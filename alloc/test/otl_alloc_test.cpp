1c86d93dba5582c629226cc326d2037eed2115dc
newwy <78290250@qq.com>
2013-09-02 07:30:56 -0700

    add gtest frame and makefile

---
+++ alloc/test/otl_alloc_test.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "otl_alloc.h"
+TEST(alloc, test_equal)
+{
+	
+}
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
